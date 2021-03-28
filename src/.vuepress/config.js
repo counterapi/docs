5178511f94b46e71f916e5deac561a84244c92a3
omegion <kurtulus@mail.com>
2021-03-28 17:51:01 +0200

    Add favicon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,6 +5,33 @@ module.exports = {
   description: description,
 
   head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-icon-180x180.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
+    ],
+    ["link", { rel: "shortcut icon", href: "/favicons/favicon.ico" }],
     ["meta", { name: "theme-color", content: "#0842ba" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     [
